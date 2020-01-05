29ec213d4fa73843cbe5ad896baa7fb581aa3d79
VanVDC <vanbvy@gmail.com>
2020-01-05 10:48:03 -0500

    Updates

---
+++ precache-manifest.ae974677dbbe32fad1107099c085da99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d24cef7dc04bd9d71d4e524650f4857",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "06dbb6bf17af8c76dc29",
+    "url": "/monster-rolodex/static/css/main.1492dab6.chunk.css"
+  },
+  {
+    "revision": "b62b9052603d1f962399",
+    "url": "/monster-rolodex/static/js/2.382b7515.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-rolodex/static/js/2.382b7515.chunk.js.LICENSE"
+  },
+  {
+    "revision": "06dbb6bf17af8c76dc29",
+    "url": "/monster-rolodex/static/js/main.a8247654.chunk.js"
+  },
+  {
+    "revision": "99047a773d79ca6f96f0",
+    "url": "/monster-rolodex/static/js/runtime-main.ec5f649a.js"
+  }
+]);
